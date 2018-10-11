56d8d7e3ed37a50812d825df439c336e177da662
cbsBiram <41389842+cbsBiram@users.noreply.github.com>
2018-10-11 23:57:37 +0000

    Add files via upload

--- medicaldiagnosis/settings.py
+++ medicaldiagnosis/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'medicaldiagnosis.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#setting to specify the route once a user has logged in/logged out
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
